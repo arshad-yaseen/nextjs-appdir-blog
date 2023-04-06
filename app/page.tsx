26b66cd11fe86bc0be5d0a594b18c4f680823c53
Glowit Labs <glowitsofware@gmail.com>
2023-04-06 16:40:12 +0530

    hero completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,28 @@
 import Header from "@/components/Header";
+import Link from "next/link";
+import { AiFillGithub } from "react-icons/ai";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className="flex justify-center h-screen w-full">
-      <div className="w-[70%] h-full flex justify-center">
+    <main className={styles.main}>
+      <div className="w-[70%] h-full flex flex-col items-center">
         <Header />
+        <div className="w-full h-14 mt-14 flex items-center justify-center">
+          <Link
+            href="https://github.com/arshad-yaseen/nextjs-appdir-blog"
+            target="_blank"
+          >
+            <div className="py-2 px-3 text-sm transition-colors text-[#4A5562] bg-white border rounded-full shadow-md border-[#D1D5DA] hover:bg-[#F3F4F6] flex items-center">
+              <AiFillGithub className="text-xl mr-2" /> Star on GitHub
+            </div>
+          </Link>
+        </div>
+        <div className="w-full h-full">
+          <h1 className="text-6xl text-center mt-10 font-bold ">
+            Next.js 13 app folder <br /> Blog with Contentlayer
+          </h1>
+        </div>
       </div>
     </main>
   );
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,22 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
-import * as Logo from 'geist/logos';
-
 
 function Header() {
   return (
-    <div className="w-full h-24 border-b-2 flex">
-      <div className="w-1/2 h-full flex items-center justify-start">
+    <header className="w-full h-24 border-b-2 flex px-6">
+      <Link href="/" className="w-1/2 h-full flex items-center justify-start">
         <Image src="/next.svg" alt="next.js svg" width={100} height={100} />
         <h1 className="font-medium ml-2  text-[1.800rem]">BLOG</h1>
-      </div>
-      <div className="w-1/2 h-full flex items-center justify-end">
-      <Logo.VercelMark size={16} />
-      </div>
-    </div>
+      </Link>
+      <Link
+        href="https://vercel.com"
+        target="_blank"
+        className="w-1/2 h-full flex items-center justify-end"
+      >
+        <Image src="/vercel.svg" alt="vercel svg" width={30} height={30} />
+      </Link>
+    </header>
   );
 }
 
